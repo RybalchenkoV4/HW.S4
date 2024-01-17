8272d71c4a26b770e5fcbfef99409ab518381796
Vladimir Rybalchenko <rybalchenko.v4@gmail.com>
2024-01-17 12:45:23 +0300

    add task 2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Задайте массив заполненный случайными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int [] array = new int[10];
+
+void GetArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100,1000);
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+}
+
+int CountEvenNumbers()
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i]%2==0) 
+        { 
+            count++; 
+        }
+    }
+    return count;
+}
+
+GetArray();
+PrintArray();
+System.Console.WriteLine("Четных чисел: " + CountEvenNumbers());
