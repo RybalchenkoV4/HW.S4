18e86aaa8dfcb00959d10af23caecb1872456cb4
Vladimir Rybalchenko <rybalchenko.v4@gmail.com>
2024-01-17 12:58:59 +0300

    add task 3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
+
+int [] array = new int[10];
+
+void GetArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100,1000);
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+}
+
+void FlipAndPrintArray()
+{
+    int [] arr = new int[array.Length];
+    int temp;
+    for (int i = 0; i < array.Length/2; i++)
+    {
+        temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+    }
+    foreach (int elem in array)
+    {
+        System.Console.Write(elem + " ");
+    }
+}
+
+GetArray();
+PrintArray();
+FlipAndPrintArray();
